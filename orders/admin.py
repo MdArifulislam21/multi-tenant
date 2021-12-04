b3e0d9535d536b5675f7e366a2c9b8cc4f6e9874
MdArifulislam21 <arifuldesigner2014@gmail.com>
2021-12-04 18:17:44 +0600

    first commit

---
+++ orders/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from tenants.utils import set_tenant_schema_for_request
+from .models import Poll, Choice
+
+from tenants.utils import tenant_from_request
+from tenants.utils import tenant_schema_from_request
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    fields = ["question", "created_by", "pub_date", 'tenant']
+    readonly_fields = ["pub_date"]
+
+    def get_queryset(self, request, *args, **kwargs):
+        tenant_schema_from_request(self.request)
+        queryset = super().get_queryset(request, *args, **kwargs)
+        tenant = tenant_from_request(request)
+        queryset = queryset.filter(tenant=tenant)
+        return queryset
+
+    def save_model(self, request, obj, form, change):
+        set_tenant_schema_for_request(self.request)
+        tenant = tenant_from_request(request)
+        obj.tenant = tenant
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Choice)
